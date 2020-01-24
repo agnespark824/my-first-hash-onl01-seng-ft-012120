4529f21f954a4e804796382d198ea6098cab13b5
Agnes Park <agnespark824@gmail.com>
2020-01-24 15:10:19 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -26,7 +26,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+  shipping_manifest["oil paintings"]
 end
 
 def adding
@@ -44,4 +44,5 @@ def adding
 
 
   # return the shipping_manifest hash below
+  shipping_manifest
 end
